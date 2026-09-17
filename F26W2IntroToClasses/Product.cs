f41e8a3f76cff305eeba892854c8da230d9f3137
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-17 13:32:59 -0400

    class

---
+++ F26W2IntroToClasses/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F26W2IntroToClasses
+{
+    public class Product
+    {
+        // fields
+        private int id;
+        private string _name;
+        private double _price;
+
+
+        // constructors
+        public Product()
+        {
+            id = 0;
+            _name = "";
+            _price = 0;
+        }
+
+        public Product(int id, string name, double price)
+        {
+            this.id = id;
+            _name = name;
+            _price = price;
+        }
+    }
+}
